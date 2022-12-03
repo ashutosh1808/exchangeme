2ccd0b12765c56e069f7e33c0c0a86004664de1f
Ashutosh Mishra <93077081+ashutosh1808@users.noreply.github.com>
2022-12-03 14:31:11 +0530

    added into new repo

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template,request
+from forex_python.converter import CurrencyRates,CurrencyCodes
+
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+	return render_template("index.html")
+
+@app.route("/convert")
+def convert():
+	try:
+		amt=float(request.args.get("amt"))
+		from_currency=request.args.get("from_currency")
+		to_currency=request.args.get("to_currency")
+		c=CurrencyRates()
+		c1=CurrencyCodes()
+		res=c.convert(from_currency,to_currency,amt)
+		res=round(res,0)
+		msg=str(c1.get_symbol(from_currency))+str(amt)+" = "+str(c1.get_symbol(to_currency))+str(res)
+		return render_template("index.html",msg=msg)
+	except Exception as e:
+		return render_template("index.html",msg=e)
+
+
+if __name__=="__main__":
+	app.run(debug=True,use_reloader=True)
