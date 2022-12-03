ae01c0ff2bde1e16c381204901e9dcb92b14565e
Ashutosh Mishra <ashutm18802@gmail.com>
2022-12-03 13:43:53 +0530

    up and running

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template
-from forex_python.converter import CurrencyRates
+from flask import Flask,render_template,request
+from forex_python.converter import CurrencyRates,CurrencyCodes
 
 
 app=Flask(__name__)
@@ -8,6 +8,20 @@ app=Flask(__name__)
 def home():
 	return render_template("home.html")
 
+@app.route("/convert")
+def convert():
+	try:
+		amt=float(request.args.get("amt"))
+		from_currency=request.args.get("from_currency")
+		to_currency=request.args.get("to_currency")
+		c=CurrencyRates()
+		c1=CurrencyCodes()
+		res=c.convert(from_currency,to_currency,amt)
+		res=round(res,0)
+		msg=str(c1.get_symbol(from_currency))+str(amt)+" = "+str(c1.get_symbol(to_currency))+str(res)
+		return render_template("home.html",msg=msg)
+	except Exception as e:
+		return render_template("home.html",msg=e)
 
 
 if __name__=="__main__":
